ef605f0772e3d35633fc38f08df4386442115eaf
javex <florian.ruechel@gmail.com>
2013-03-26 00:51:31 +0100

    Finished first rev

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Wunderlist Task Exporter
 
 Introduction
 ------------
-
 I wrote this to export my tasks in an easy format from the Wunderlist page.
-Configure then run it with `./wunderlist_parser.py`.
+The Wunderlist API is not documented but they provide helpful error messages
+and have a clean structure (for those parts I used at least). My script does
+only a bare minimum but should be enough to give everyone an idea of how it
+works.
 
 
 Configuration
@@ -17,3 +19,32 @@ Configuration
 
 Enter username and password.
 
+
+Usage
+-----
+Just run it with `python wunderlist_parser.py` and it will take some time to
+load all lists and their tasks. The final data will be organized as a dict
+with one entry for each list. The key is the internal ID (really only
+important for matching task and list) and the value is a tuple: The first
+element is the title of the list, the second one is a list of all tasks in
+that list.
+
+The tasks themselves are organized as classes as they are somewhat complex and
+that makes it easier to write an for them. Take a look at the `Task` class to
+see what is accessible and how (the names really don't need any explanation).
+
+Currently, the script just spits out the final result at the end. If you want
+something different, you can actually build on top of that: Delete the printing
+lines and just import the file as a module. Then access 
+`wunderlist_parser.lists` and you have your result: All tasks of all lists,
+including those already completed.
+
+
+Contribute
+----------
+This is only a **very** basic script that just achieves what I needed: A clean
+data representation to organize the data. If you need more functionality, just
+add it. I will then be happy to incorporate your changes here.
+
+If you clean up the module to make it less dirty, I will also gladly include
+it.
--- wunderlist_parser.py
+++ wunderlist_parser.py
@@ -1,6 +1,90 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import requests
+import json
+from collections import OrderedDict
+from datetime import datetime
+from pprint import pprint
 from wunderlist_config import USER, PASSWORD
 
-print(USER, PASSWORD)
+session = requests.Session()
+
+login_data = json.dumps({'email': USER, 'password': PASSWORD})
+r = session.post('https://api.wunderlist.com/login', login_data)
+ret = r.json()
+session.headers['Authorization'] = 'Bearer {0}'.format(ret['token'])
+
+ops = json.dumps({'ops': [{"method": "get", "url": "/me/lists"}],
+                   "sequential": True})
+
+
+r = session.post('https://api.wunderlist.com/batch', ops)
+data = r.json()
+task_list_raw = data["results"][0]["body"]
+
+task_list = [(d["position"], 
+  d["id"],
+  d["title"]) for d in task_list_raw]
+
+task_list.append((1.0, 'inbox', 'Inbox'))
+task_list = sorted(task_list, key=lambda t: t[0])
+lists = OrderedDict((t[1], (t[2], [])) for t in task_list)
+pprint(lists)
+
+
+ops = json.dumps({'ops': [{"method": "get", "url": "/inbox/tasks"}], 
+                  "sequential": True})
+r = session.post('https://api.wunderlist.com/batch', ops)
+data = r.json()
+
+
+def from_iso8601(s):
+    return datetime.strptime(s, "%Y-%m-%dT%H:%M:%SZ")
+
+class Task(object):
+
+    def __init__(self, api_data):
+
+        date_list = ["completed_at", "created_at", "due_date"]
+        for date_obj in date_list:
+            try:
+                setattr(self, date_obj, from_iso8601(api_data[date_obj]))
+            except TypeError:
+                setattr(self, date_obj, None)
+        self.id = api_data["id"]
+        self.list_id = api_data["list_id"]
+        self.local_identifier = api_data["local_identifier"]
+        self.note = api_data["note"]
+        self.position = api_data["position"]
+        self.starred = api_data["starred"]
+        self.title = api_data["title"]
+        self._all_data = api_data
+
+    @property
+    def completed(self):
+        return bool(self.completed_at)
+
+    def __str__(self):
+        return "{0}: {1}".format(self.list_id, self.title)
+
+    def __repr__(self):
+        return str(self)
+
+
+all_tasks = []
+for task in data["results"][0]["body"]:
+    all_tasks.append(Task(task))
+
+ops = json.dumps({'ops': [{"method": "get", "url": "/me/tasks"}],
+                  'sequential': True})
+r = session.post('https://api.wunderlist.com/batch', ops)
+data = r.json()
+
+for task in data["results"][0]["body"]:
+    all_tasks.append(Task(task))
+
+for task in all_tasks:
+    l = lists[task.list_id]
+    l[1].append(task)
+
+pprint(lists)
